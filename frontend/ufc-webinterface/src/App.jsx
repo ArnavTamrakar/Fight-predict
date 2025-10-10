d530e1fb1cc2225430284b3b23f0a87588c252c6
Arnav <arnavtamrakar7@gmail.com>
2025-10-10 20:03:38 +1030

    front end changes

--- frontend/ufc-webinterface/src/App.jsx
+++ frontend/ufc-webinterface/src/App.jsx
@@ -20,12 +20,23 @@ function App() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <FighterInput label="Fighter 1" onSelect={setFighter1} />
-      <FighterInput label="Fighter 2" onSelect={setFighter2} />
-      <button type="submit">Predict</button>
+    <FighterInput label="Fighter 1" onSelect={setFighter1} />
+    <FighterInput label="Fighter 2" onSelect={setFighter2} />
+    <button type="submit">Predict</button>
 
-      {result && <pre>{JSON.stringify(result, null, 2)}</pre>}
-    </form>
+    {result && (
+      <div className='results'>
+        <h3>Fight Prediction</h3>
+        <p>{fighter1} win Probability: {(result.prediction.probabilities[0] * 100).toFixed(1)}%</p>
+        <p>{fighter2} win Probability: {(result.prediction.probabilities[1] * 100).toFixed(1)}%</p>
+        <p>
+          Winner: <strong>
+            {result.prediction.winner === "Fighter 1" ? fighter1 : fighter2}
+          </strong>
+        </p>
+      </div>
+    )}
+  </form>
   );
 }
 
